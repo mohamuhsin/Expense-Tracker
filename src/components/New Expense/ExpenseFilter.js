5649cac2f2f54ca43a87ddf5a82c309a1b92e4f8
mohamuhsin <muhsinohoho@outlook.com>
2023-03-14 18:31:17 +0300

    created a button change lister

--- src/components/New Expense/ExpenseFilter.js
+++ src/components/New Expense/ExpenseFilter.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import './ExpenseFilter.css';
+import React from "react";
+import "./ExpenseFilter.css";
 
 const ExpenseFilter = () => {
-    return(
-        <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
+  const [date, setDate] = useState("");
+  const buttonChangeHandler = () => {
+    setDate("selectedDate");
+    console.log(date);
+  };
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select>
-          <option value='2022'>2023</option>
-          <option value='2021'>2022</option>
-          <option value='2020'>2021</option>
-          <option value='2019'>2020</option>
+          <option value="2022" onChange={buttonChangeHandler}>
+            2023
+          </option>
+          <option value="2021" onChange={buttonChangeHandler}>
+            2022
+          </option>
+          <option value="2020" onChange={buttonChangeHandler}>
+            2021
+          </option>
+          <option value="2019" onChange={buttonChangeHandler}>
+            2020
+          </option>
         </select>
       </div>
     </div>
-    )
-}
+  );
+};
 
 export default ExpenseFilter;
